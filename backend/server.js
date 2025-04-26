698151c65b8014d5bae80779939824940f236f1b
Niharrakholiya <nihar.rakholiya526@gmail.com>
2025-04-26 15:26:39 +0530

    changes in backend

--- backend/server.js
+++ backend/server.js
@@ -26,15 +26,39 @@ const app = express();
 app.use(cors({
   origin: ["http://localhost:3000", "http://localhost:5173", "http://localhost:5174","https://leafcare-production.up.railway.app"], // Allow both ports
   credentials: true, 
-  allowedHeaders: ["Content-Type", "Authorization"],
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
+  allowedHeaders: [
+    "Content-Type", 
+    "Authorization",
+    "Access-Control-Allow-Origin",
+    "Access-Control-Allow-Credentials"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  exposedHeaders: ["set-cookie"]
+
 }));
+
+app.options('*', cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    return res.status(200).json({});
+  }
+  next();
+});
+
 app.use(
     helmet({
       crossOriginOpenerPolicy: { policy: "same-origin-allow-popups" }, // Allow popups
       crossOriginEmbedderPolicy: false // Disable to prevent iframe issues
     })
   );
+
 app.use(bodyParser.json());
 
 // Routes
